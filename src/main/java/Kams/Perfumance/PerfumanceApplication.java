58baed574a34b7305ee8de61ff100a8e3e15572e
Kams <kams1011@naver.com>
2021-06-28 22:55:26 +0900

    Initial commit

---
+++ src/main/java/Kams/Perfumance/PerfumanceApplication.java
@@ -0,0 +1,13 @@
+package Kams.Perfumance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfumanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfumanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Kams/Perfumance/PerfumanceApplicationTests.java
@@ -0,0 +1,13 @@
+package Kams.Perfumance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfumanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
