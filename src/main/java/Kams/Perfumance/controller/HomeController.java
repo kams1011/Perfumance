09f9a6954c8da72f01a9f2c5610c2c926265333e
Kams <kams1011@naver.com>
2021-07-07 16:59:22 +0900

    스프링 시큐리티 재적용 // gitignore test

--- src/main/java/Kams/Perfumance/controller/HomeController.java
+++ src/main/java/Kams/Perfumance/controller/HomeController.java
@@ -2,17 +2,18 @@ package Kams.Perfumance.controller;
 
 
 import Kams.Perfumance.service.UserServiceImpl;
+import Kams.Perfumance.vo.UserVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
 
 
 @Controller
 public class HomeController {
-
-
     //Restcontroller로 하면 String만 반환되는 이유??
 
     private final UserServiceImpl userService;
@@ -22,43 +23,36 @@ public class HomeController {
         this.userService=userService;
     }
 
-//    @GetMapping("user")
-//    public String user(@ModelAttribute UserVo user, Model model){
-//        return "/home/user";
-//    }
-//
     @GetMapping("select")
     public void selectTest(){
         System.out.println(userService.getAllUser());
     }
-//
-//
-//    @GetMapping("insert")
-//    public void insertTest(){
-//        Date date = new Date();
-//        UserVo uvo = UserVo.builder()
-//                .id("4")
-//                .pwd("4")
-//                .nick("$")
-//                .email("4")
-//                .img("4")
-//                .regdt(date)
-//                .deldt(date)
-//                .dealnum(0)
-//                .build();
-//        userService.Register(uvo);
-//    }
+
+
+    @GetMapping("insert")
+    public void insertTest(){
+        Date date = new Date();
+        UserVo uvo = UserVo.builder()
+                .id("4")
+                .pwd("4")
+                .nick("$")
+                .email("4")
+                .img("4")
+                .regdt(date)
+                .deldt(date)
+                .dealnum(0)
+                .build();
+        userService.Register(uvo);
+    }
 
     @GetMapping("/user")
     public String User(Model model){
-        model.addAttribute("list",userService.getAllUser());
+
+
+        model.addAttribute("list",  userService.getAllUser().get(2).getId());
 
         return "home/user";
     }
 
-    @GetMapping("s")
-    public String s(){
-        return "s";
-    }
 
 }
