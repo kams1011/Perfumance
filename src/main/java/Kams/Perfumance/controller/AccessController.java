85b7749d1ef9ba179786f57273ddadc75f3fc4bf
Kams <kams1011@naver.com>
2021-09-12 02:24:57 +0900

    SprincSecurity 구현 중// groovy 에러 발생해서 자바 버전 다운그레이드로 수정함. css파일은 여전히 적용 안되는걸 봐서 자바 버전 문제는 아닌듯.

--- src/main/java/Kams/Perfumance/controller/AccessController.java
+++ src/main/java/Kams/Perfumance/controller/AccessController.java
@@ -1,4 +1,18 @@
 package Kams.Perfumance.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/access")
 public class AccessController {
+
+
+    @GetMapping("/login")
+    public String login(){
+
+
+        return "access/login";
+    }
 }
